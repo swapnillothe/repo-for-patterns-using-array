87f3c438eba10c8c6f6f122bba16320822bffdcd
swapnillothe <44019530+swapnillothe@users.noreply.github.com>
2018-10-27 16:20:40 +0530

    Change the file name in the test.

--- testFiles/run_all_test.sh
+++ testFiles/run_all_test.sh
@@ -2,7 +2,7 @@
 
 ./run_test.sh ../src/rectangleGenerator.js ../appTestData/input/all_rectangle_input ../appTestData/output/all_rectangle_output
 Result1=$?
-./run_test.sh ../src/diamondPattern.js ../appTestData/input/all_diamond_input ../appTestData/output/all_diamond_output
+./run_test.sh ../src/diamondGenerator.js ../appTestData/input/all_diamond_input ../appTestData/output/all_diamond_output
 Result2=$?
 ./run_test.sh ../src/triangleGenerator.js ../appTestData/input/all_triangle_input ../appTestData/output/all_triangle_output
 Result3=$?
